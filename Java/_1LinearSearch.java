050efa0ee3ef63510d0eef5e3d72986e3d6caa6c
Prashant Borde <bordeprashant12@gmail.com>
2025-05-19 23:38:07 +0530

    linear search program

---
+++ Java/_1LinearSearch.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class _1LinearSearch{
+    public static int linearsearch (int[] arr,int key){
+        for(int i=0;i<arr.length;i++){
+            if(key==arr[i]){
+                return(i);
+            }
+        }
+        return -1;
+    }
+public static void main(String[]args){
+    Scanner sc= new Scanner(System.in);
+    System.out.print("Enter Size of Array : ");
+    int size = sc.nextInt();
+    int arr[] = new int[size];
+    for(int i=0;i<size; i++){
+        arr[i] = sc.nextInt();
+    }
+    System.out.print("What you want to Search : ");
+    int key=sc.nextInt();
+    key=linearsearch(arr,key);
+    System.out.println("Your Key found at : "+ key);
+}
+}
