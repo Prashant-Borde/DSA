5e0e3661755a22cf6520647e6d521eae0f7180af
Prashant Borde <bordeprashant12@gmail.com>
2025-06-03 17:22:21 +0530

    new unique string code

---
+++ JavaScript/_31uniquechar.js
@@ -0,0 +1,13 @@
+function unique(str) {
+    let newstr = "";
+    for (let i = 0; i < str.length; i++) {
+        if (!newstr.includes(str[i])) {
+            newstr += str[i];
+        }
+    }
+    return newstr;
+}
+
+let str = "abcdefghabcd";
+let newstr = unique(str);
+console.log(newstr); // Output: abcdefgh
