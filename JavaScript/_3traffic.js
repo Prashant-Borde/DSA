15c86f7b6a72f2e899b5f75c982ed19df8ae00ef
Prashant Borde <bordeprashant12@gmail.com>
2025-06-05 16:52:07 +0530

    old code renamed

---
+++ JavaScript/_3traffic.js
@@ -0,0 +1,11 @@
+let light = 'red';
+
+if (light === 'red') {
+    console.log("Stop");
+} else if (light === 'yellow') {
+    console.log("Slow");
+} else if (light === 'green') {
+    console.log("Go");
+} else {
+    console.log("Undefined");
+}
